b11e336a00c44ac93cf636ef0653b3c491bed872
Igor <114025058+igaryakqwe@users.noreply.github.com>
2024-06-24 12:31:29 +0300

    Initialize repository

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+import '@material/button/dist/mdc.button.min.css';
+
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  )
+}
+
+export default App
+
